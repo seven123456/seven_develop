03a172c46de4eb7923004756be67cb43eddbbb98
WRGHO-20171OO\Administrator <956971053@qq.com>
2018-01-04 18:02:45 +0800

    导入依赖

--- app/src/main/java/com/seven/seven/ui/MainActivity.java
+++ app/src/main/java/com/seven/seven/ui/MainActivity.java
@@ -4,13 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.seven.seven.R;
+import com.seven.seven.common.utils.StatusBarUtil;
+import com.seven.seven.ui.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends BaseActivity {
+    private MainActivity mActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        StatusBarUtil.setTranslate(mActivity,true);
     }
 }
--- app/src/main/java/com/seven/seven/ui/base/BaseActivity.java
+++ app/src/main/java/com/seven/seven/ui/base/BaseActivity.java
@@ -1,10 +1,23 @@
 package com.seven.seven.ui.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.seven.seven.common.utils.StatusBarUtil;
+
 /**
  * Created  on 2018-01-04.
  * author:seven
  * email:seven2016s@163.com
  */
 
-public class BaseActivity {
+public  abstract class BaseActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContextViewResId());
+    }
+
+    protected abstract int getContextViewResId();
 }
