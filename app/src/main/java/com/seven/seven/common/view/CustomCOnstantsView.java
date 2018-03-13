6623466d08b1822db0c10609d9cb51af05e1383f
WRGHO-20171OO\Administrator <956971053@qq.com>
2018-03-13 17:59:28 +0800

    抽取可扩展toolbar为一个组件

--- app/src/main/java/com/seven/seven/common/view/CustomCOnstantsView.java
+++ app/src/main/java/com/seven/seven/common/view/CustomCOnstantsView.java
@@ -1,10 +1,14 @@
 package com.seven.seven.common.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.seven.seven.R;
@@ -15,9 +19,21 @@ import com.seven.seven.R;
  * email:seven2016s@163.com
  */
 
-public class CustomCOnstantsView extends RelativeLayout{
+public class CustomCOnstantsView extends RelativeLayout {
 
     private Toolbar toolbar;
+    private Context mContext;
+    private ImageView imageBar;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private CustomfinshPagerOnclickListener pagerOnclickListener;
+
+    public void getCustomfinshPagerOnclickListener(CustomfinshPagerOnclickListener customfinshPagerOnclickListener) {
+        pagerOnclickListener = customfinshPagerOnclickListener;
+    }
+
+    public interface CustomfinshPagerOnclickListener {
+        void customfinshPager();
+    }
 
     public CustomCOnstantsView(Context context) {
         super(context);
@@ -25,6 +41,7 @@ public class CustomCOnstantsView extends RelativeLayout{
 
     public CustomCOnstantsView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mContext = context;
         initView(context, attrs);
 
     }
@@ -35,14 +52,65 @@ public class CustomCOnstantsView extends RelativeLayout{
     }
 
     private void initView(Context context, AttributeSet attrs) {
-        LayoutInflater.from(context).inflate(R.layout.layout_constants,this,true);
+        LayoutInflater.from(context).inflate(R.layout.layout_constants, this, true);
         toolbar = findViewById(R.id.toolbar);
         initToolbar();
+        imageBar = findViewById(R.id.image_view);
+        collapsingToolbarLayout = findViewById(R.id.collapsing);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.constantsView);
+        if (typedArray != null) {
+            /*toolbar的文字*/
+            String toolbarTitle = typedArray.getString(R.styleable.constantsView_toolbarTitle);
+            toolbar.setTitle(toolbarTitle);
+            /*toolbar折叠后的颜色*/
+            int color = typedArray.getColor(R.styleable.constantsView_toolbarCollapsingColor, getResources().getColor(R.color.colorPrimary));
+            collapsingToolbarLayout.setContentScrimColor(color);
+            /*折叠后title的颜色*/
+            int titleColor = typedArray.getColor(R.styleable.constantsView_toolbarTitleCollapsingColor, getResources().getColor(R.color.red));
+            collapsingToolbarLayout.setCollapsedTitleTextColor(titleColor);
+        }
+
+        toolbar.setNavigationOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pagerOnclickListener.customfinshPager();
+            }
+        });
     }
 
     private void initToolbar() {
-     /*   setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
+        ((AppCompatActivity) mContext).setSupportActionBar(toolbar);
+        ((AppCompatActivity) mContext).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    /*
+    * 设置toolbar的文字
+    * */
+    public void setToolbarTitle(String resId) {
+        if (resId != null) {
+            collapsingToolbarLayout.setTitle(resId);
+        }
+    }
+
+    /*
+    * 设置toolbar的文字颜色 接收的colorint，而不是resources
+    * */
+    public void setToolbarTitleColor(int resId) {
+        collapsingToolbarLayout.setCollapsedTitleTextColor(resId);
+    }
+
+    /*
+    * 设置toolbar折叠后的颜色
+    *
+    * */
+    public void setToolbarCollapsingColor(int resId) {
+        collapsingToolbarLayout.setContentScrimColor(resId);
+    }
+    /*
+    * 设置要渐变的imageview
+    * */
+
+    public void setImageView(int resId) {
+        imageBar.setImageResource(resId);
+    }
 }
--- app/src/main/java/com/seven/seven/ui/base/fragment/EFragment.java
+++ app/src/main/java/com/seven/seven/ui/base/fragment/EFragment.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 import com.seven.seven.R;
 import com.seven.seven.ui.base.activity.ConstantsLayout;
+import com.seven.seven.ui.base.activity.ViewTestActivity;
 
 /**
  * Created  on 2018-02-05.
@@ -21,14 +22,14 @@ public class EFragment extends BaseFragment {
 
     @Override
     protected void initView() {
-//        TextView textView = rootView.findViewById(R.id.text);
-//        textView.setText("我是E");
-//        textView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(getActivity(),ConstantsLayout.class));
-//            }
-//        });
+        TextView textView = rootView.findViewById(R.id.text);
+        textView.setText("我是E");
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),ViewTestActivity.class));
+            }
+        });
     }
 
     @Override
